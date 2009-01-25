0544ee537aabab2cf4e9353c5b4f748c4401392f
Suraj N. Kurapati <sunaku@gmail.com>
2009-01-25 11:01:33 -0800

    add release notes for version 2.0.1

--- lib/rumai.rb
+++ lib/rumai.rb
@@ -3,8 +3,8 @@ gem 'inochi', '~> 0'
 require 'inochi'
 
 Inochi.init :Rumai,
-  :version => '2.0.0',
-  :release => '2008-02-04',
+  :version => '2.0.1',
+  :release => '2009-01-25',
   :website => 'http://snk.tuxfamily.org/lib/rumai',
   :tagline => 'Ruby interface to the wmii window manager'
 
