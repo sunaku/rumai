d7686f8e28dba0786ad9ee2ba15eb40845cd0e02
Suraj N. Kurapati <sunaku@gmail.com>
2009-04-14 11:54:09 -0700

    upgrade to Inochi 1.0.0

--- Rakefile
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-gem 'inochi', '~> 0'
-require 'inochi'
-
-Inochi.rake :Rumai,
-  :rubyforge_project => 'sunaku',
-  :upload_target => File.expand_path('~/www/lib/rumai/'),
-  :upload_delete => true
--- lib/rumai.rb
+++ lib/rumai.rb
@@ -1,11 +1,11 @@
 require 'rubygems'
-gem 'inochi', '~> 0'
+gem 'inochi', '~> 1'
 require 'inochi'
 
 Inochi.init :Rumai,
   :version => '2.0.2',
   :release => '2009-02-26',
-  :website => 'http://snk.tuxfamily.org/lib/rumai',
+  :website => 'http://snk.tuxfamily.org/lib/rumai/',
   :tagline => 'Ruby interface to the wmii window manager'
 
 require 'rumai/fs'
