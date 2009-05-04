d75399c433bfcd662ad859dd24bf49adc4569bd5
Suraj N. Kurapati <sunaku@gmail.com>
2009-05-03 21:12:20 -0700

    switch from Minitest to Dfect

--- lib/rumai.rb
+++ lib/rumai.rb
@@ -6,7 +6,10 @@ Inochi.init :Rumai,
   :version => '2.0.2',
   :release => '2009-02-26',
   :website => 'http://snk.tuxfamily.org/lib/rumai/',
-  :tagline => 'Ruby interface to the wmii window manager'
+  :tagline => 'Ruby interface to the wmii window manager',
+  :develop => {
+    :dfect => nil, # for unit testing
+  }
 
 require 'rumai/fs'
 require 'rumai/wm'
--- test/rumai/ixp/message.rb
+++ test/rumai/ixp/message.rb
@@ -1,23 +1,9 @@
 require 'pp' if $DEBUG
 
-##
-# XXX: using an explicit class instead of Kernel#describe()
-#      because, in Ruby 1.8, constants referenced in the blocks
-#      are resolved in the scope where the block was defined, not
-#      inside the class in which the block is being class_eval()ed.
-#
-#      Thankfully, this bug is fixed in Ruby 1.9! :-)
-#      See this article for examples and proof:
-#       http://www.pgrs.net/2007/9/12/ruby-constants-have-weird-behavior-in-class_eval/
-#
-#
-# NOTE: this affects us because miniunit's Kernel#describe()
-#       method evaluates its block using class_eval()
-#
-class The_IXP_library < MiniTest::Spec
+class << Object.new
   include Rumai::IXP
 
-  before do
+  D .< do
     unless defined? @conn
       # connect to the wmii IXP server
       require 'socket'
@@ -36,12 +22,12 @@ class The_IXP_library < MiniTest::Spec
       :msize   => Tversion::MSIZE,
       :version => Tversion::VERSION
     ) do |req, rsp|
-      rsp.type.must_equal Rversion.type
-      rsp.version.must_equal req.version
+      T { rsp.type == Rversion.type }
+      T { rsp.version == req.version }
     end
   end
 
-  it 'can read a directory' do
+  D 'can read a directory' do
     transaction 'attach to FS root', Tattach.new(
       :tag   => 0,
       :fid   => 0,
@@ -49,14 +35,14 @@ class The_IXP_library < MiniTest::Spec
       :uname => ENV['USER'],
       :aname => ''
     ) do |req, rsp|
-      rsp.type.must_equal Rattach.type
+      T { rsp.type == Rattach.type }
     end
 
     transaction 'stat FS root', Tstat.new(
       :tag => 0,
       :fid => 0
     ) do |req, rsp|
-      rsp.type.must_equal Rstat.type
+      T { rsp.type == Rstat.type }
     end
 
     transaction 'open the FS root for reading', Topen.new(
@@ -64,7 +50,7 @@ class The_IXP_library < MiniTest::Spec
       :fid  => 0,
       :mode => Topen::OREAD
     ) do |req, rsp|
-      rsp.type.must_equal Ropen.type
+      T { rsp.type == Ropen.type }
     end
 
     transaction 'fetch a Stat for every file in FS root', Tread.new(
@@ -73,7 +59,7 @@ class The_IXP_library < MiniTest::Spec
       :offset => 0,
       :count  => Tversion::MSIZE
     ) do |req, rsp|
-      rsp.type.must_equal Rread.type
+      T { rsp.type == Rread.type }
 
       if $DEBUG
         s = StringIO.new(rsp.data, 'r')
@@ -90,7 +76,7 @@ class The_IXP_library < MiniTest::Spec
       :tag    => 0,
       :fid    => 0
     ) do |req, rsp|
-      rsp.type.must_equal Rclunk.type
+      T { rsp.type == Rclunk.type }
     end
 
     transaction 'closed fid should not be readable', Tread.new(
@@ -99,11 +85,11 @@ class The_IXP_library < MiniTest::Spec
       :offset => 0,
       :count  => Tversion::MSIZE
     ) do |req, rsp|
-      rsp.type.must_equal Rerror.type
+      T { rsp.type == Rerror.type }
     end
   end
 
-  it 'can read & write a file' do
+  D 'can read & write a file' do
     transaction 'attach to /', Tattach.new(
       :tag   => 0,
       :fid   => 0,
@@ -111,7 +97,7 @@ class The_IXP_library < MiniTest::Spec
       :uname => ENV['USER'],
       :aname => ''
     ) do |req, rsp|
-      rsp.type.must_equal Rattach.type
+      T { rsp.type == Rattach.type }
     end
 
     file = %W[rbar temp#{$$}]
@@ -124,7 +110,7 @@ class The_IXP_library < MiniTest::Spec
       :newfid => 1,
       :wname => root
     ) do |req, rsp|
-      rsp.type.must_equal Rwalk.type
+      T { rsp.type == Rwalk.type }
     end
 
     transaction "create #{leaf.inspect}", Tcreate.new(
@@ -134,14 +120,14 @@ class The_IXP_library < MiniTest::Spec
       :perm => 0644,
       :mode => Topen::ORDWR
     ) do |req, rsp|
-      rsp.type.must_equal Rcreate.type
+      T { rsp.type == Rcreate.type }
     end
 
     transaction "close the fid for #{root.inspect}", Tclunk.new(
       :tag => 0,
       :fid => 1
     ) do |req, rsp|
-      rsp.type.must_equal Rclunk.type
+      T { rsp.type == Rclunk.type }
     end
 
     transaction "walk to #{file.inspect} from /", Twalk.new(
@@ -150,14 +136,14 @@ class The_IXP_library < MiniTest::Spec
       :newfid => 1,
       :wname => file
     ) do |req, rsp|
-      rsp.type.must_equal Rwalk.type
+      T { rsp.type == Rwalk.type }
     end
 
     transaction 'close the fid for /', Tclunk.new(
       :tag => 0,
       :fid => 0
     ) do |req, rsp|
-      rsp.type.must_equal Rclunk.type
+      T { rsp.type == Rclunk.type }
     end
 
     transaction "open #{file.inspect} for writing", Topen.new(
@@ -165,7 +151,7 @@ class The_IXP_library < MiniTest::Spec
       :fid  => 1,
       :mode => Topen::ORDWR
     ) do |req, rsp|
-      rsp.type.must_equal Ropen.type
+      T { rsp.type == Ropen.type }
     end
 
     write_req, write_rsp = transaction "write to #{file.inspect}", Twrite.new(
@@ -174,8 +160,8 @@ class The_IXP_library < MiniTest::Spec
       :offset => 0,
       :data   => "hello world!!!"
     ) do |req, rsp|
-      rsp.type.must_equal Rwrite.type
-      rsp.count.must_equal req.data.length, 'count man?'
+      T { rsp.type == Rwrite.type }
+      T { rsp.count == req.data.length }
     end
 
     transaction "verify stuff was written to #{file.inspect}", Tread.new(
@@ -184,29 +170,27 @@ class The_IXP_library < MiniTest::Spec
       :offset => 0,
       :count  => write_rsp.count
     ) do |req, rsp|
-      rsp.type.must_equal Rread.type
-      rsp.data.must_equal write_req.data
+      T { rsp.type == Rread.type }
+      T { rsp.data == write_req.data }
     end
 
     transaction "remove #{file.inspect}", Tremove.new(
       :tag => 0,
       :fid => 1
     ) do |req, rsp|
-      rsp.type.must_equal Rremove.type
+      T { rsp.type == Rremove.type }
     end
 
     transaction "fid for #{file.inspect} should have been closed by Tremove", Tclunk.new(
       :tag => 0,
       :fid => 1
     ) do |req, rsp|
-      rsp.type.must_equal Rerror.type
+      T { rsp.type == Rerror.type }
     end
   end
 
-  private
-
   # Transmits the given request and returns the received response.
-  def send_and_recv request
+  def self.send_and_recv request
     # send the request
       if $DEBUG
         puts
@@ -226,24 +210,19 @@ class The_IXP_library < MiniTest::Spec
       end
 
       if response.type == Rerror.type
-        response.must_be_kind_of Rerror
+        T { response.kind_of? Rerror }
       else
-        response.type.must_equal request.type + 1
+        T { response.type == request.type + 1 }
       end
 
-      response.tag.must_equal request.tag
+      T { response.tag == request.tag }
 
     response
   end
 
-  def transaction description, request
-    begin
-      response = send_and_recv(request)
-      yield request, response if block_given?
-      [request, response]
-    rescue MiniTest::Assertion => e
-      e.message << "\n\n\t task: #{description}\n\n\t with: #{request.class}"
-      raise e
-    end
+  def self.transaction description, request
+    response = send_and_recv(request)
+    yield request, response if block_given?
+    [request, response]
   end
 end
