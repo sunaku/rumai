c9178c329a63d9dba60cafad440ba7b2bfda07dd
Suraj N. Kurapati <sunaku@gmail.com>
2009-11-17 00:18:42 -0800

    Version 3.1.1

--- lib/rumai.rb
+++ lib/rumai.rb
@@ -8,8 +8,8 @@ gem 'inochi', '~> 1'
 require 'inochi'
 
 Inochi.init :Rumai,
-  :version => '3.1.0',
-  :release => '2009-10-02',
+  :version => '3.1.1',
+  :release => '2009-11-16',
   :website => 'http://snk.tuxfamily.org/lib/rumai/',
   :tagline => 'Ruby interface to the wmii window manager',
   :develop => {
